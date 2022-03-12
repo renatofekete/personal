5a21f63e29b09ad7ad15305d846bf4ee3236d3a7
Renato Fekete <renatofekete.91@gmail.com>
2022-03-12 01:19:26 +0100

    added accessibility to switch

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,9 @@ const Header = () => {
   return (
     <header>
       <nav className={styles.navbar}>
-        <label tabIndex={0} className={styles.switch} htmlFor="switch">
-          <input tabIndex={1} className={styles.input} name="switch" id="switch" onChange={e => changeTheme(e)} type="checkbox"/>
-          <span tabIndex={2} className={styles.slider}></span>  
+        <label className={styles.switch} htmlFor="switch">
+          <input defaultChecked={false} className={styles.input} name="switch" id="switch" onChange={e => changeTheme(e)} type="checkbox"/>
+          <span className={styles.slider}></span>  
         </label>
       </nav>
     </header>
