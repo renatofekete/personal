9495528ef701ea2e12f22031b9c13a0cab51dd99
Renato Fekete <renatofekete.91@gmail.com>
2022-03-17 21:39:20 +0100

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
+import { createClient } from 'contentful'
 
-const Home: NextPage = () => {
+export const getStaticProps =  async () => {
+
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_SPACE_ID as string,
+    accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN as string,
+  })
+
+  const res = await client.getEntries({
+    content_type: 'project'
+  })
+
+  return {
+    props: {
+      projects: res.items
+    }
+  }
+}
+
+const Home: NextPage = ({projects} : any) => {
+  console.log(process.env.SPACE_ID) 
+  console.log(projects)
   
+
   return (
     <>
       <Head>
@@ -18,6 +40,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>Hi, I'm Renato!</h1>
         <p>And this is my place where I'll store projects and where I'll play around with technology that I want to learn.</p>
         <p>This website is a constant work in progress, and I'll change it a lot.</p>
+        <p>{process.env.NEXT_PUBLIC_SPACE_ID}</p>
       </main>
 
       <footer>
---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const projects: NextPage = () => {
+  return (
+    <>
+      <h1>Hello from Projects page</h1>
+    </>
+  )
+}
+
+export default projects
